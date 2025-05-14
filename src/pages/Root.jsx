f1ee0a866b1baa4712c413190a4725b86597fccb
riaurko <riaurko@gmail.com>
2025-05-14 23:45:31 +0600

    Implemented Footer

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import Navbar from "../layouts/Navbar";
 import { Outlet } from "react-router";
+import Footer from "../layouts/Footer";
 
 const Root = () => {
     return (
         <Fragment>
             <Navbar />
             <Outlet />
+            <Footer />
         </Fragment>
     );
 };
